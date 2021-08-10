faed409b68f8fe600b1b8e4b1629ced13ab17147
hashmat00 <hashmat00@aol.com>
2021-08-10 12:22:16 -0700

    added instruction how to add second vm

--- main.tf
+++ main.tf
@@ -16,3 +16,17 @@ module "vm1" {
 }
 
 #call again the module to create second vm with different name and RG but vnet/vnetRG/subnet will be same
+# module "vm2" {
+#   source              = "./modules/vm"
+#   # source              = "Azure/vnet/azurerm"
+#   rg-name =     "hm-test1"
+#   vm-name =      "hm-vm1"
+#   vnet-rg =     "test"
+#   vnet  =       "test-vnet"
+#   subnet =      "default"
+#   tags = {
+#     environment = "test"
+#     project   = "AVM"
+#     owner     = "kla"
+#    }
+# }
